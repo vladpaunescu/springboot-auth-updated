77b86d6719704fd3c8cdf181c561d1ba1e32ec4d
Vlad Paunescu <vlad.paunescu@gmail.com>
2020-02-24 02:35:16 +0200

    Added spring security from tutorial

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
@@ -1,10 +1,17 @@
 package com.auth0.samples.authapi.springbootauthupdated;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootAuthUpdatedApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAuthUpdatedApplication.class, args);
